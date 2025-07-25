47ef42cded5b98e1aedb907485483f250beb07e4
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-07-25 21:20:16 +0700

    finished convert space project (as far as removing line breaks)

--- jQuery/js_convert_space.js
+++ jQuery/js_convert_space.js
@@ -1,22 +1,55 @@
-// material referenced: https://stackoverflow.com/questions/50157296/how-to-obtain-data-from-random-wikipedia-page-with-jquery (answered by Andrew L)
-// material refrenced: https://codepen.io/loweehahn/pen/LZYxwj by Kelvin
-// material referenced: https://www.reddit.com/r/webdev/comments/y1a2qa/why_does_the_page_keeps_refreshing/ yea it was an issue thanks reddit
+// material referenced: https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+// material refrenced: https://www.codiga.io/blog/display-code-snippets-in-html/
 
+function displayText(txt) {
+    $('#displaytext').show();
+    $("#displaytext").append(txt);
+}
+
+function displayCode(txt) {
+    var newtxt = txt.replace("<", "&lt;");
+    newtxt = newtxt.replace(">", "&gt;");
+
+    console.log(newtxt);
+
+    // var code = $("<code></code>").text(newtxt);
+    var pre = $("<pre></pre>").text(txt);
+
+    $('#convertedtext').show();
+    $("#convertedtext").append(pre);
+}
+
+function copyText() {
+    $('#convertedtext').select();
+    $('#convertedtext').setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText($('#convertedtext').val());
+}
 
 $(document).ready(function(){
+    $('#displaytext').hide();
+    $('#convertedtext').hide();
 
     $('form').submit(function(e) {
         e.preventDefault();
 
         // clear articles
-        // $('#articles').html('');
+        $('#displaytext').html('');
+        $('#convertedtext').html('');
 
         // get number of articles to generate
-        var $input = $(e.target).find('input');
+        var $input = $(e.target).find('textarea');
         var txt = $input.val();
 
         console.log(txt);
+        
+        // remove linebreaks
+        var newtxt = txt.replace(/(\r\n|\n|\r)/gm, "<br>");
 
+        console.log(newtxt);
+
+        displayText(newtxt);
+        displayCode(newtxt);
     })
 
 });
